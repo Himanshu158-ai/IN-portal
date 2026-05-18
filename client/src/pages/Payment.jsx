f3eb682359e57c8f43fadfecb601baa71e0397df
Himanshu Singh <hs5317610@gmail.com>
2026-05-18 19:43:52 +0530

    refactor: remove redundant and unused configuration files

--- client/src/pages/Payment.jsx
+++
@@ -1,6 +0,0 @@
-﻿import React from 'react'
-
-export default function Payment() {
-  return <div>Payment Page</div>
-}
-
--- client/src/pages/UploadDocuments.jsx
+++
@@ -1,6 +0,0 @@
-﻿import React from 'react'
-
-export default function UploadDocuments() {
-  return <div>Upload Documents</div>
-}
-
